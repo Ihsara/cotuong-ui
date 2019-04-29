4f32bb34c6b5fd36941ee350a468e7f611de0baa
Ihsara <longchau21@gmail.com>
2019-04-29 05:05:50 +0300

    Fix minor spellings

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
-
-let main_container = document.querySelector('.main_container');
-
 function generateSquare(square){
     return `<li class="square" data-card="${square}" style="grid-column:${square%10}/ span 1; grid-row: ${Math.floor(square/10)}/ span 1;"><i class="fa ${square}">${square}</i></li>`;
 }
 
 function initBoard(){ 
+  let main_container = document.querySelector('.main_container');
   let squareHTML = [...Array(110).keys()].slice(11, 110).filter(s => s%10 !== 0).map(sq => generateSquare(sq));
-  
   main_container.innerHTML = squareHTML.join('');
 }
 
 initBoard(); 
+console.log("Done!")
